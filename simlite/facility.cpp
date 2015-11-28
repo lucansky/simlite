40ac9ba88f562bd5f0690fc06fd81220dc2cf572
Tomas Kello <kello.tomas@gmail.com>
2015-11-28 19:02:32 +0100

    facility prerobene, konstruktor bere parameter CAPACITY a dokaze sa spravat aj ako STORE

--- simlite/facility.cpp
+++ simlite/facility.cpp
@@ -1,94 +1,144 @@
 #include "facility.h"
 
-Facility::Facility() {
+Facility::Facility(unsigned int capacity) {
+	// TODO preco musi byt this-> a nie this. (prekladac pisal chybu)
+	assert(capacity > 0);
+	this->capacity = capacity;
+	itemsIn = 0;
 	// save for Output
-		empty = true;
 		eventStart = Time_t;
-		maxQu = 0;
+		maxQuComming  = 0;
+		maxQuOutgoing = 0;
 	// End save for Output
 }
 
-void Facility::seize(Process &p) {
-	if (empty == true)
+void Facility::seize(Process &p, unsigned int itemSize) {
+	if (capacity - itemsIn >= itemSize && quComming.empty())
 	{
 		// save for Output
-			empty = false;
-			duration_of_empty += Time_t - eventStart;
+			histogramQu.push_back(HistogramQu{ Time_t - eventStart, itemsIn });
 			eventStart = Time_t;
 		// End save for Output
 
-		duration_in_queue.push_back(0);
+		itemsIn += itemSize;
+		//duration_in_quComming.push(0);
 		p.ActivateNext();
+
+		// ak prisli zasoby tak zavolaj dalsieho do skladu na zobratie
+		if (quOutgoing.front().itemSize <= itemsIn && !quOutgoing.empty())
+		{
+			// duration_in_quOutgoing.push_back( ........ );
+			itemsIn -= quOutgoing.front().itemSize;
+			quOutgoing.front().p->ActivateNext();
+			quOutgoing.pop();
+		}
 	}
 	else
 	{
-		qu.push(FacilityQuItem{ &p, Time_t });
+		quComming.push(FacilityQuItem{ &p, Time_t, itemSize });
 
 		// save for Output
-			if (maxQu < qu.size())
-				maxQu = qu.size();
+			if (maxQuComming < quComming.size())
+				maxQuComming = quComming.size();
 		// End save for Output
 	}
 }
 
-void Facility::release() {
-	duration_of_service.push_back(Time_t - eventStart);
-	if (!qu.empty())
+void Facility::release(Process &p, unsigned int itemSize) {
+	if (quOutgoing.empty() && itemsIn >= itemSize)
 	{
 		// save for Output
+			histogramQu.push_back(HistogramQu{ Time_t - eventStart, itemsIn });
 			eventStart = Time_t;
-			duration_in_queue.push_back(Time_t - qu.front().waitStart);
 		// End save for Output
 
-		qu.front().p->ActivateNext();
-		qu.pop();
+		//duration_in_quOutgoing.push_back(0);
+		itemsIn -= itemSize;
+		p.ActivateNext();
+
+		// ak sa uvolnilo tak zavolaj dalsieho do skladu na vlozenie
+		if (quComming.front().itemSize <= capacity - itemsIn && !quComming.empty())
+		{
+			// duration_in_quComming.push( ........ );
+			itemsIn += quComming.front().itemSize;
+			quComming.front().p->ActivateNext();
+			quComming.pop();
+		}
 	}
 	else
 	{
+		// must waitng, queue not empty, or not enought itemsIn
+		quOutgoing.push(FacilityQuItem{ &p, Time_t, itemSize });
+
 		// save for Output
-			empty = true;
-			eventStart = Time_t;
+			if (maxQuOutgoing < quOutgoing.size())
+				maxQuOutgoing = quOutgoing.size();
 		// End save for Output
 	}
 }
 
 void Facility::Output() {
-	if (empty)
+
+	histogramQu.push_back(HistogramQu{ Time_t - eventStart, itemsIn });
+	if (itemsIn != 0)
 	{
-		duration_of_empty += Time_t - eventStart;
-	}
-	else
-	{
-		cout << "\n\033[1;31mFacility not empty!!\n";
-		if (!qu.empty())
-			cout << "And still have " << qu.size() << " Process in queue\n";
+		cout << "\n\033[1;31mFacility has " << itemsIn << " items!!\n";
+		if (!quComming.empty())
+			cout << "And have " << quComming.size() << " records in quComming\n";
+		if (!quOutgoing.empty())
+			cout << "And have " << quOutgoing.size() << " records in quOutgoing\n";
 		cout <<"\033[0m\n";
 	}
-	cout << "Maximalna fronta: " << maxQu << "\n";
+	cout << "Max queue: In " << maxQuComming
+	   << "\n          Out " << maxQuOutgoing << "\n";
 
+	// imcomming queue
 	double sum=0; int count=0;
-	while (!duration_in_queue.empty())
+	while (!duration_in_quComming.empty())
 	{
 		count++;
-		sum += duration_in_queue.back();
-		duration_in_queue.pop_back();
+		sum += duration_in_quComming.front();
+		duration_in_quComming.pop_back();
 	}
 	if (count == 0)
-		cout << "Vo fronte nebol ziadny zakaznik\n";
+		cout << "Noone was in incomming queue\n";
 	else
-		cout << "Priemerna cas vo fronte: " << sum / count << "\n";
+		cout << "Average time in incomming queue: " << sum / count << "\n";
 
-	sum = 0; count = 0;
-	while (!duration_of_service.empty())
+	// imcomming queue
+	sum=0; count=0;
+	while (!duration_in_quOutgoing.empty())
 	{
 		count++;
-		sum += duration_of_service.back();
-		duration_of_service.pop_back();
+		sum += duration_in_quOutgoing.front();
+		duration_in_quOutgoing.pop_back();
 	}
 	if (count == 0)
-		cout << "Obsluha neobsluzila ziadneho zakaznika\n";
+		cout << "No one was in outgoing queue\n";
 	else
-		cout << "Priemerna dlzka obsluhy: " << sum / count << "\n";
+		cout << "Average time in outgoing queue: " << sum / count << "\n";
 
-	cout << "Zariadenie volne: " << duration_of_empty << "\n";
+	if (capacity >= 1) // TODO asi netreba
+	{
+		sum = 0; count = 0;
+		if (histogramQu.empty())
+			cout << "No one enter facility\n";
+		else
+		{
+			unsigned int duration_of_empty = 0;
+			while (!histogramQu.empty())
+			{
+				if (histogramQu.front().itemsIn == 0)
+					duration_of_empty += histogramQu.front().durationTime;
+				else
+				{
+					sum += histogramQu.front().durationTime * histogramQu.front().itemsIn;
+				}
+				histogramQu.pop_back();
+			}
+			cout << "Average usage of facility: " << sum /(Time_tStart - Time_t) << "\n";
+			cout << "Facility free: " << duration_of_empty << " -- " << duration_of_empty /(Time_tStart - Time_t) << "% \n"; // v percentach
+		}
+
+	}
 }
--- simlite/facility.h
+++ simlite/facility.h
@@ -8,22 +8,37 @@
 typedef struct {
 	Process *p;
 	double waitStart;
+	unsigned int itemSize;
 } FacilityQuItem;
+
+typedef struct {
+	double durationTime;
+	unsigned int itemsIn;
+} HistogramQu;
 
 class Facility {
 private:
 	double eventStart;
-	bool empty;
-	double duration_of_empty;
-	std::queue<FacilityQuItem> qu;
-	int maxQu;
-	std::vector<double> duration_of_service;
-	std::vector<double> duration_in_queue;
+	unsigned int itemsIn;
+	unsigned int capacity;
+
+	unsigned int maxQuComming;
+	std::queue<FacilityQuItem> quComming;  // waiting to seize
+
+	// + for store
+	unsigned int maxQuOutgoing;
+	std::queue<FacilityQuItem> quOutgoing; // waiting to release
+
+
+	// output
+	std::vector<HistogramQu> histogramQu;
+	std::vector<double> duration_in_quComming;
+	std::vector<double> duration_in_quOutgoing;
 public:
 	void Output();
-	Facility();
-	void seize(Process &p);
-	void release();
+	Facility(unsigned int capacity = 1);
+	void seize(Process &p, unsigned int itemSize = 1);
+	void release(Process &p, unsigned int itemSize = 1);
 };
 
 #endif //SIMLITE_FACILITY_H
