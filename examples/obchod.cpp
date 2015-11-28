14f593c695333e77d558a7e5b62746957d8d877f
Adam Lucansky <adamlucansky@gmail.com>
2015-11-28 22:34:35 +0100

    Obchod s generatorom ako process

--- examples/obchod.cpp
+++ examples/obchod.cpp
@@ -36,47 +36,58 @@ public:
 
 };
 
+class Generator : Process {
+public:
+    Generator() {
+        GenerujZakaznika();
+    }
+
+    void GenerujZakaznika() {
+        new Zakaznik;
+        ActivateAfter(15, DoAfter(GenerujZakaznika));
+    }
+};
 
 
 // ............................................................ generator.h ...................
-class Generator : public Process{
-private:
-    double (*delayFunc)(double);
-    double funcArg;
-public:
-    //virtual void Behavior(); // potrebne aby tuto metodu definoval uzivatel
-    void Behavior();
-    Generator(double (*delayFunc)(double), double funcArg);
-    void Run();
-};
-// ............................................................ END generator.h ...................
-
-
-// priklad ako definovat Behavior generatora. Toto by mal definovat uzivatel
-void Generator::Behavior() {
-    new Zakaznik;
-}
-
-
-// ............................................................ generator.cpp ...................
-void Generator::Run() {
-    Behavior();
-    ActivateAfter(delayFunc(funcArg), DoAfter(Run)); // spusti generator znovu za
-    //ActivateAfter(20, DoAfter(Run)); // spusti generator znovu za
-}
-
-Generator::Generator(double (*delayFunc)(double), double funcArg) {
-    this->delayFunc = delayFunc;
-    this->funcArg = funcArg;
-    ActivateAfter(delayFunc(0), DoAfter(Run)); // spusti generator znovu za
-    //Run();
-}
+//class Generator : public Process{
+//private:
+//    double (*delayFunc)(double);
+//    double funcArg;
+//public:
+//    //virtual void Behavior(); // potrebne aby tuto metodu definoval uzivatel
+//    void Behavior();
+//    Generator(double (*delayFunc)(double), double funcArg);
+//    void Run();
+//};
+//// ............................................................ END generator.h ...................
+//
+//
+//// priklad ako definovat Behavior generatora. Toto by mal definovat uzivatel
+//void Generator::Behavior() {
+//    new Zakaznik;
+//}
+//
+//
+//// ............................................................ generator.cpp ...................
+//void Generator::Run() {
+//    Behavior();
+//    ActivateAfter(delayFunc(funcArg), DoAfter(Run)); // spusti generator znovu za
+//    //ActivateAfter(20, DoAfter(Run)); // spusti generator znovu za
+//}
+//
+//Generator::Generator(double (*delayFunc)(double), double funcArg) {
+//    this->delayFunc = delayFunc;
+//    this->funcArg = funcArg;
+//    ActivateAfter(delayFunc(0), DoAfter(Run)); // spusti generator znovu za
+//    //Run();
+//}
 // ............................................................ END generator.cpp ...................
 
 
 int main() {
     Init(0, 100);
-    new Generator(Exp, 20);
+    new Generator();
     Run();
 
     pokladna.Output();
